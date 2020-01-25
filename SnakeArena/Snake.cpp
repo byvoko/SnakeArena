cc3a16b04d0f61cd9deff6fb33ed548a4a789582
Tomáš Vít <vit@cutter.cz>
2020-01-25 16:43:19 +0100

    - jeste opravy

--- SnakeArena/Snake.cpp
+++ SnakeArena/Snake.cpp
@@ -5,10 +5,9 @@ Snake::Snake(Color color, sf::Vector2u startPosition, size_t bodyLen):
 	mDirection (Direction::Up),
 	mBodyLen (bodyLen)
 {
-	mPositions.push_back(startPosition);
-	for (size_t i = 0; i < bodyLen; i++)
+	for (int i = 0; i < bodyLen; i++)
 	{
-		mPositions.push_back({ startPosition.x, startPosition.y + 1 });
+		mPositions.push_back({ startPosition.x, startPosition.y + i });
 	}
 }
 
