8afcdc015d4f459b8df0d08c4a94f9e93a0d140e
Štěpán Odstrčil <stepan.odstrcil@puxdesign.cz>
2020-01-25 15:10:35 +0100

    Add stuff

--- SnakeArena/Snake.hpp
+++ SnakeArena/Snake.hpp
@@ -1,8 +1,31 @@
 #pragma once
 
+#include <SFML/Graphics.hpp>
+
 class Snake
 {
 private:
+	struct Position
+	{
+		int x, y;
+	};
+
+	enum Direction
+	{
+		Up,
+		Right,
+		Down,
+		Left,
+	};
+
+	std::vector<Position> mPositions;
 	int mColor;
+	Direction mDirection;
+
+	sf::Sprite mBody;
+
+public:
+	Snake();
+	virtual ~Snake();
 };
 
